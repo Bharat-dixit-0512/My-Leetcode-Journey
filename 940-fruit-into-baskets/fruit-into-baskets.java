a36d264f55d7d8ba0b5382f63f01f0af04826b32
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-02-17 14:21:18 +0530

    Time: 8 ms (92.72%) | Memory: 74.4 MB (6.27%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int[] fr = new int[100007];
+        int max = 0;
+        int type = 0;
+        int start = 0;
+        for (int i = 0; i < fruits.length; i++) {
+            int f = fruits[i];
+            if (fr[f] == 0) type++;
+            fr[f]++;
+            while (type > 2) {
+                fr[fruits[start]]--;
+                if (fr[fruits[start]] == 0) type--;
+                start++;
+            }
+            max = Math.max(max, i - start + 1);
+        }
+        return max;
+    }
+}
