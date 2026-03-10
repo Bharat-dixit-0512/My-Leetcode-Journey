5f480ea9b5dbe8188a9e01558b42f93442a728ca
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-03-10 20:25:28 +0530

    Time: 3 ms (96.06%) | Memory: 42.2 MB (47.96%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.java
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -1,5 +1,13 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
+        //pehle Nearest sq. root value find kro c ki 
+        //then 0 se uss value tkk ke answer tkk binary search apply kro
+        //agr sum chota aata toh left pointer increment kro
+        //otherwise right pointer decrement kro 
+        //agr equal hojaye toh true
+        //agr true na aaye toh false;
+
+
         if(c<0)return false;
 
         long left=0;
