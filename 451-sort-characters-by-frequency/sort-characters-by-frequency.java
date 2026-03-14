21940b09684aaa53470ec4c9a59cd5fcf8acac91
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-03-14 21:23:12 +0530

    Time: 13 ms (74.21%) | Memory: 46.8 MB (51.51%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,24 +1,29 @@
 class Solution {
     public String frequencySort(String s) {
-        Map<Character,Integer> map=new HashMap<>();
-        for(char c:s.toCharArray()){
-            map.put(c,map.getOrDefault(c,0)+1);
+        Map<Character,Integer> map = new HashMap<>();
+        for(char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
+        List<Integer> list = new ArrayList<>();
+        for(Map.Entry<Character,Integer> entry : map.entrySet()){
+            list.add(entry.getValue());
+        }
+        Collections.sort(list, Collections.reverseOrder());
 
-        List<Character> [] bucket = new List[s.length() + 1];
-        for (char key : map.keySet()) {
-            int frequency = map.get(key);
-            if (bucket[frequency] == null) bucket[frequency] = new ArrayList<>();
-            bucket[frequency].add(key);
-        }
-				
         StringBuilder sb = new StringBuilder();
-        for (int pos = bucket.length - 1; pos >= 0; pos--)
-            if (bucket[pos] != null)
-                for (char c : bucket[pos])
-                    for (int i = 0; i < pos; i++)
-                        sb.append(c);
+        Set<Character> used = new HashSet<>();
 
+        for(int freq : list){
+            for(Map.Entry<Character,Integer> entry : map.entrySet()){
+                if(entry.getValue() == freq && !used.contains(entry.getKey())){
+                    char a = entry.getKey();
+                    used.add(a);
+                    for(int j = 0; j < freq; j++){
+                        sb.append(a);
+                    }
+                }
+            }
+        }
         return sb.toString();
     }
 }
