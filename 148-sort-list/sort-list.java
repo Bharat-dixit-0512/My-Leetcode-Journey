83bc73651074a15df8be044573761401d154cd30
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-03-21 17:07:11 +0530

    Time: 10 ms (54.15%) | Memory: 59 MB (95.30%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -1,39 +1,41 @@
 class Solution {
     public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) return head;
-
-        ListNode prev = null, slow = head, fast = head;
-        while (fast != null && fast.next != null) {
-            prev = slow;
-            slow = slow.next;
-            fast = fast.next.next;
+        if(head==null || head.next==null)return head;
+        ListNode mid=getmid(head);
+        ListNode left=sortList(head);
+        ListNode right=sortList(mid);
+        return merge(left,right);
+    }
+    private ListNode getmid(ListNode head){
+        ListNode slow=head,fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        prev.next = null; 
-
-        ListNode l1 = sortList(head);
-        ListNode l2 = sortList(slow);
-
-        return merge(l1, l2);
+        ListNode mid=slow.next;
+        slow.next=null;
+        return mid;
     }
 
     private ListNode merge(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-
+        ListNode m = new ListNode(0);
+        ListNode curr = m;
         while (l1 != null && l2 != null) {
             if (l1.val < l2.val) {
-                curr.next = l1;   
+                curr.next = l1;
                 l1 = l1.next;
             } else {
-                curr.next = l2;   
+                curr.next = l2;
                 l2 = l2.next;
             }
             curr = curr.next;
         }
-
-        if (l1 != null) curr.next = l1;
-        if (l2 != null) curr.next = l2;
-
-        return dummy.next;
+        if (l1 != null) {
+            curr.next = l1;
+        }
+        if (l2 != null) {
+            curr.next = l2;
+        }
+        return m.next;
     }
 }
