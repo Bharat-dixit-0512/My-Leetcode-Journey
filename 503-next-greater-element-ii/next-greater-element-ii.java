ea7614ccaf0f0ce6af7411440549036f3c7e5794
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-03-30 09:12:28 +0530

    Time: 93 ms (7.19%) | Memory: 46.9 MB (99.76%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
-        for (int i = 0; i < n; i++) {
-            result[i] = -1;
-            for (int j = 1; j < n; j++) {
-                int nextIndex = (i + j) % n;
-                if (nums[nextIndex] > nums[i]) {
-                    result[i] = nums[nextIndex];
+        int n=nums.length;
+        int[]result=new int[n];
+        for(int i=0;i<n;i++){
+            result[i]=-1;
+            for(int j=1;j<n;j++){
+                int nidx=(i+j)%n;
+                if(nums[nidx]>nums[i]){
+                    result[i]=nums[nidx];
                     break;
                 }
             }
