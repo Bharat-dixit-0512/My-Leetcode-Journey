cf95561532e9591f4e80fef9276a46c3ba9af402
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-04-01 23:21:27 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (79.88%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.java
+++ 861-flipping-an-image/flipping-an-image.java
@@ -5,8 +5,7 @@ class Solution {
         }
         for(int i=0;i<image.length;i++){
             for(int j=0;j<image[0].length;j++){
-                if(image[i][j]== 1)image[i][j]=0;
-                else image[i][j]=1;
+                image[i][j] ^= 1;
             }
         }
         return image;
