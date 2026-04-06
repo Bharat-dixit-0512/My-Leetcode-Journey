986fcd877600d47e5168613759091ee01b0f6911
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-04-06 09:11:20 +0530

    Time: 29 ms (85.37%) | Memory: 146.2 MB (75.80%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,22 +1,22 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if (k <= 1) return nums;
-        int n = nums.length;
-        int[] result = new int[n - k + 1];
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        
-        for (int i = 0; i < n; i++) {
-            pq.offer(new int[]{nums[i], i});
-            
-            if (i >= k - 1) {
-                while (pq.peek()[1] <= i - k) {
-                    pq.poll();
-                }
-                result[i - k + 1] = pq.peek()[0];
+        int n=nums.length;
+        Deque<Integer> dq=new ArrayDeque<>();
+        int ans[]=new int[n-k+1];
+
+        for(int i=0;i<n;i++){
+            while(!dq.isEmpty() && dq.peekFirst()<i-k+1){
+                dq.pollFirst();
+            }
+            while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i]){
+                dq.pollLast();
+            }
+            dq.addLast(i);
+
+            if(i>=k-1){
+                ans[i-k+1]=nums[dq.peekFirst()];
             }
         }
-        
-        return result;
+        return ans;
     }
 }
