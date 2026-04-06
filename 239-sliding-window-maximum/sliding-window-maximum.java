5637022eabb068efe089ccc03caf3d88f821f7a3
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-04-06 08:20:33 +0530

    Time: 76 ms (13.95%) | Memory: 158.4 MB (7.88%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (k <= 1) return nums;
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        
+        for (int i = 0; i < n; i++) {
+            pq.offer(new int[]{nums[i], i});
+            
+            if (i >= k - 1) {
+                while (pq.peek()[1] <= i - k) {
+                    pq.poll();
+                }
+                result[i - k + 1] = pq.peek()[0];
+            }
+        }
+        
+        return result;
+    }
+}
