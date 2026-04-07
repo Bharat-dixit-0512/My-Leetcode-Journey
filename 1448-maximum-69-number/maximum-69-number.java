76a77cfbc7e5c8922ef70e7cf0579bafb44a9bb4
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-04-07 15:15:17 +0530

    Time: 1 ms (80.86%) | Memory: 42.3 MB (36.27%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -1,16 +1,16 @@
 class Solution {
     public int maximum69Number (int num) {
         int x=num;
-        HashMap<Integer,Integer> freq=new HashMap<>();
+        // HashMap<Integer,Integer> freq=new HashMap<>();
 
-        while(x!=0){
-            int rem=x%10;
-            freq.put(rem,freq.getOrDefault(rem,0)+1);
-            x/=10;
-        }
-        if(freq.size()==1 && freq.containsKey(9)){
-            return num;
-        }
+        // while(x!=0){
+        //     int rem=x%10;
+        //     freq.put(rem,freq.getOrDefault(rem,0)+1);
+        //     x/=10;
+        // }
+        // if(freq.size()==1 && freq.containsKey(9)){
+        //     return num;
+        // }
 
         String s = String.valueOf(num);
         char[] arr = s.toCharArray();
