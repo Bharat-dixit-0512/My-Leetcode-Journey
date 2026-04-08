937621c2b3bdf14befeb2653a0b05102aa5912ef
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-04-08 11:55:18 +0530

    Time: 3 ms (86.88%) | Memory: 47.7 MB (59.28%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,21 +1,24 @@
 class Solution {
     public int candy(int[] ratings) {
-        int[] arr = new int[ratings.length];
-        Arrays.fill(arr, 1);
-        for (int i = 1; i < arr.length; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                arr[i] = arr[i - 1] + 1;
+        int n=ratings.length;
+        int[] candies = new int[n];
+        for(int i=0;i<n;i++){
+            candies[i]=1;
+        }
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                candies[i]=candies[i-1]+1;
             }
         }
-        for (int i = arr.length - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                arr[i] = Math.max(arr[i], arr[i + 1] + 1);
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                candies[i]=Math.max(candies[i],candies[i+1]+1);
             }
         }
-        int sum = 0;
-        for (int i : arr) {
-            sum += i;
+        int tot=0;
+        for(int i=0;i<n;i++){
+            tot+=candies[i];
         }
-        return sum;
+        return tot;
     }
 }
