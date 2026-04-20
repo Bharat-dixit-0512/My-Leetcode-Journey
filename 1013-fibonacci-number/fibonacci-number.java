4a896b7968536ac41c799c40656b327cda09a4e4
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-04-20 13:54:34 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (72.30%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -4,7 +4,9 @@ class Solution {
         return fibonacci(n,dp);
     }
     int fibonacci(int n,int dp[]){
-        if(n<=1)return n;
+        // if(n<=1)return n;
+        if(n==0)return 0;
+        if(n==1)return 1;
         if(dp[n]!=0)return dp[n];
 
         return dp[n]= fibonacci(n-1,dp)+fibonacci(n-2,dp);
