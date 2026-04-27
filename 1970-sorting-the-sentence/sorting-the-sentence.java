2584deaf386fe129a3f13f2b29b60cb11aad6b2c
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-04-27 10:35:40 +0530

    Time: 1 ms (38.91%) | Memory: 42.8 MB (66.57%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.java
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -1,41 +1,15 @@
 class Solution {
-    public String sortSentence(String str) {
-        String arr[]=str.split("\\s+");
-        String res[]=new String[arr.length];
-        for(int i=0;i<arr.length;i++){
-            String s=arr[i];
-            if(s.charAt(s.length()-1)=='1'){
-                res[0]=s.substring(0,s.length()-1);
-            }
-            if(s.charAt(s.length()-1)=='2'){
-                res[1]=s.substring(0,s.length()-1);
-            }
-            if(s.charAt(s.length()-1)=='3'){
-                res[2]=s.substring(0,s.length()-1);
-            }
-            if(s.charAt(s.length()-1)=='4'){
-                res[3]=s.substring(0,s.length()-1);
-            }
-            if(s.charAt(s.length()-1)=='5'){
-                res[4]=s.substring(0,s.length()-1);
-            }
-            if(s.charAt(s.length()-1)=='6'){
-                res[5]=s.substring(0,s.length()-1);
-            }
-            if(s.charAt(s.length()-1)=='7'){
-                res[6]=s.substring(0,s.length()-1);
-            }
-            if(s.charAt(s.length()-1)=='8'){
-                res[7]=s.substring(0,s.length()-1);
-            }
-            if(s.charAt(s.length()-1)=='9'){
-                res[8]=s.substring(0,s.length()-1);
-            }
+    public String sortSentence(String s) {
+        String[] arr = s.split("\\s+");
+        int n = arr.length;
+        String[] res = new String[n];
+        for(int i=0; i<n; i++){
+            String word = arr[i];
+            int idx = word.charAt(word.length()-1)-'0'-1;
+            String str = word.substring(0,word.length()-1);
+            res[idx] = str;
         }
-        StringBuilder stb=new StringBuilder();
-        for(String st:res){
-            stb.append(st).append(" ");
-        }
-        return stb.toString().trim();
+        return String.join(" ", res);
+
     }
 }
