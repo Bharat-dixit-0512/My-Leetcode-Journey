899d11fddb49b6c05f133ac3eb38915084d9ae9b
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-04-29 11:05:33 +0530

    Time: 446 ms (5.00%) | Memory: 47 MB (6.76%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -10,15 +10,16 @@ class Solution {
         return false;
     }
     private boolean fun(char board[][],String word,int i,int j){
+        if(word.length()==0) return true;
         if(i<0 || i>=board.length || j<0 || j>=board[0].length)return false;
         if(board[i][j]!=word.charAt(0)) return false;
-
-        word=word.substring(1);
-        if(word.length()==0) return true;
-
         char ch=board[i][j];
         board[i][j]='#';
-        boolean res=(fun(board,word, i+1, j) || fun(board,word, i-1, j) || fun(board,word, i, j+1) || fun(board,word, i, j-1));
+        boolean res=false;
+        res|=fun(board,word.substring(1), i+1, j);
+        res|=fun(board,word.substring(1), i-1, j);
+        res|=fun(board,word.substring(1), i, j+1);
+        res|=fun(board,word.substring(1), i, j-1);
         board[i][j]=ch;
         return res;
     }
