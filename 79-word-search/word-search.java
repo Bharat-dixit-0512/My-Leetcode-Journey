2d895feb6916531dac6130727b5dafed0a9cedcf
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-04-29 11:07:03 +0530

    Time: 131 ms (68.27%) | Memory: 43 MB (60.38%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -2,25 +2,18 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
-                if(board[i][j]==word.charAt(0)){
-                    if(fun(board,word,i,j))return true;
-                }
+                if(fun(board,word,i,j,0))return true;
             }
         }
         return false;
     }
-    private boolean fun(char board[][],String word,int i,int j){
-        if(word.length()==0) return true;
-        if(i<0 || i>=board.length || j<0 || j>=board[0].length)return false;
-        if(board[i][j]!=word.charAt(0)) return false;
-        char ch=board[i][j];
-        board[i][j]='#';
-        boolean res=false;
-        res|=fun(board,word.substring(1), i+1, j);
-        res|=fun(board,word.substring(1), i-1, j);
-        res|=fun(board,word.substring(1), i, j+1);
-        res|=fun(board,word.substring(1), i, j-1);
-        board[i][j]=ch;
+    private boolean fun(char[][] board,String word,int start,int end,int idx){
+        if(idx==word.length())return true;
+        if(start<0 || start>=board.length || end<0 || end>=board[0].length || board[start][end]!=word.charAt(idx))return false;
+        char c=board[start][end];
+        board[start][end]='#';
+        boolean res=fun(board,word,start+1,end,idx+1)||fun(board,word,start-1,end,idx+1)||fun(board,word,start,end+1,idx+1)||fun(board,word,start,end-1,idx+1);
+        board[start][end]=c;
         return res;
     }
 }
