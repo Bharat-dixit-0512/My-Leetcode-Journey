1842a01778cc7705e6f3cc8bf7a0e0bac8806242
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-05-26 16:52:24 +0530

    Time: 17 ms (76.11%) | Memory: 82.8 MB (11.63%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,13 +1,11 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
         if ((long)m * k > bloomDay.length) return -1; 
-
         int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
         for (int day : bloomDay) {
             min = Math.min(min, day);
             max = Math.max(max, day);
         }
-
         int ans = -1;
         while (min <= max) {
             int mid = min + (max - min) / 2;
@@ -20,7 +18,6 @@ class Solution {
         }
         return ans;
     }
-
     public boolean possible(int[] arr, int day, int m, int k) {
         int count = 0, bouquets = 0;
         for (int val : arr) {
