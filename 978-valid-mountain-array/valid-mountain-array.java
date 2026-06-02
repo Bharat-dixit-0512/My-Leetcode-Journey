e7829a98bca2f33a5457dc1f36f79f1640163fec
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-06-02 23:34:02 +0530

    Time: 2 ms (85.65%) | Memory: 47.5 MB (31.12%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.java
+++ 978-valid-mountain-array/valid-mountain-array.java
@@ -4,13 +4,9 @@ class Solution {
         int n = arr.length;
         if (n < 3) return false;
         int i = 0;
-        while (i + 1 < n && arr[i] < arr[i + 1]) {
-            i++;
-        }
+        while (i + 1 < n && arr[i] < arr[i + 1])i++;
         if (i == 0 || i == n - 1) return false;
-        while (i + 1 < n && arr[i] > arr[i + 1]) {
-            i++;
-        }
+        while (i + 1 < n && arr[i] > arr[i + 1])i++;
         return i == n - 1;
     }
 }
