0b7f1fdd4bd7d9f62645f64526f6a32e957e32ad
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-06-09 19:10:22 +0530

    Time: 1 ms (87.76%) | Memory: 45.5 MB (45.87%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,19 +1,18 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result=new ArrayList<>();
         List<Integer> numbers=new ArrayList<>();
         for(int i:nums)numbers.add(i);
 
-        List<List<Integer>> result=new ArrayList<>();
         fun(numbers,result,new ArrayList<>());
         return result;
     }
-    void fun(List<Integer> nums,List<List<Integer>>ans,List<Integer> p){
+    void fun(List<Integer> nums,List<List<Integer>>ans,List<Integer>p){
         if(nums.size()==0){
             ans.add(new ArrayList<>(p));
             return;
         }
-        for(int i=0;i<nums.size();i++)
-        {
+        for(int i=0;i<nums.size();i++){
             int num=nums.get(i);
             p.add(num);
             nums.remove(i);
