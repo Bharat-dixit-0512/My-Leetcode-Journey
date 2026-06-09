f04f5778efa4b3ef2fc54f5dcc5c478e1e10293a
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-06-09 19:17:44 +0530

    Time: 24 ms (13.12%) | Memory: 98.1 MB (45.55%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,25 +1,23 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> result=new ArrayList<>();
-        List<Integer> nums=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
         for(int i=1;i<=n;i++){
-            nums.add(i);
+            list.add(i);
         }
-        fun(nums,result,k,new ArrayList<>(),0);
+        List<List<Integer>> result=new ArrayList<>();
+        fun(list,result,new ArrayList<>(),k,0);
         return result;
     }
-    void fun(List<Integer> nums,List<List<Integer>> result,int k,List<Integer>c,int si){
+    void fun(List<Integer> nums,List<List<Integer>>ans,List<Integer> c,int k,int si){
         if(c.size()==k){
-            result.add(new ArrayList<>(c));
+            ans.add(new ArrayList<>(c));
             return;
         }
-        if(si>=nums.size())return;
-        //exclude
-        fun(nums,result,k,c,si+1);
-
-        //include
-        c.add(nums.get(si));
-        fun(nums,result,k,c,si+1);
-        c.remove(c.size()-1);
+        for(int i=si;i<nums.size();i++){
+            int num=nums.get(i);
+            c.add(num);
+            fun(nums,ans,c,k,i+1);
+            c.remove(c.size()-1);
+        }
     }
 }
