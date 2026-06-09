0cc6ab6f5b8a779b9241a60791d1f25b696e22c9
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-06-09 19:21:01 +0530

    Time: 19 ms (50.46%) | Memory: 98.2 MB (13.69%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,25 +1,19 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        List<Integer> list=new ArrayList<>();
-        for(int i=1;i<=n;i++){
-            list.add(i);
-        }
-        List<List<Integer>> result=new ArrayList<>();
-        fun(list,result,new ArrayList<>(),k,0);
-        return result;
+        List<Integer> ll = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        Combine(n, k, 1, ll, ans);
+        return ans;
     }
-    void fun(List<Integer> nums,List<List<Integer>>ans,List<Integer> c,int k,int si){
-        if(c.size()==k){
-            ans.add(new ArrayList<>(c));
-            return;
-        }
-        if(si>=nums.size())return;
-        //exclude
-        fun(nums,ans,c,k,si+1);
-
-        //include
-        c.add(nums.get(si));
-        fun(nums,ans,c,k,si+1);
-        c.remove(c.size()-1);
-    }
+    public static void Combine(int n, int k, int start, List<Integer> ll,  List<List<Integer>> ans) {
+    	if(ll.size() == k) {
+			ans.add(new ArrayList<>(ll));
+			return;
+		}
+		for(int i = start; i <= n; i++) {
+			ll.add(i);
+			Combine(n, k, i + 1,ll,  ans);
+			ll.remove(ll.size() - 1);
+		}
+	}
 }
