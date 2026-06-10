822b30ecc0d9980fc2bce516478b6f7bde54c0f8
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-06-10 23:57:20 +0530

    Time: 3 ms (34.10%) | Memory: 45.9 MB (33.03%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,21 +1,20 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        solve(candidates, target, 0, new ArrayList<>(), ans);
+        List<List<Integer>> ans=new ArrayList<>();
+        fun(candidates,target,ans,0,new ArrayList<>());
         return ans;
     }
-    public void solve(int[] arr, int target, int idx,List<Integer> list, List<List<Integer>> ans) {
-        if (target == 0) {
-            ans.add(new ArrayList<>(list));
+    void fun(int[]candidates,int target,List<List<Integer>> ans,int si,List<Integer> c){
+        if(target==0){
+            ans.add(new ArrayList<>(c));
             return;
         }
-        if (target < 0) {
-            return;
-        }
-        for (int i = idx; i < arr.length; i++) {
-            list.add(arr[i]);
-            solve(arr, target - arr[i], i, list, ans);
-            list.remove(list.size() - 1);
+        if(target<0)return;
+        for(int i=si;i<candidates.length;i++){
+            int num=candidates[i];
+            c.add(num);
+            fun(candidates,target-num,ans,i,c);
+            c.remove(c.size()-1);
         }
     }
 }
