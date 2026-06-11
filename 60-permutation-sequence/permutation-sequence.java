e3be44afc9d112a50fc8a4ff7e28a1a333661f7e
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-06-11 18:50:04 +0530

    Time: 1 ms (99.55%) | Memory: 42.6 MB (93.08%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.java
+++ 60-permutation-sequence/permutation-sequence.java
@@ -1,28 +1,21 @@
 class Solution {
     public String getPermutation(int n, int k) {
-        List<Integer> num = new ArrayList<>();
-        for (int i = 1; i <= n; i++) num.add(i);
-        List<List<Integer>> ans = new ArrayList<>();
-        fun(num, k, ans, new ArrayList<>());
-        List<Integer> kthPermutation = ans.get(k - 1);
-        StringBuilder sb = new StringBuilder();
-        for (int val : kthPermutation) {
-            sb.append(val);
-        }
-        return sb.toString();
-    }
-    void fun(List<Integer> list, int k, List<List<Integer>> ans, List<Integer> p) {
-        if (list.size() == 0) {
-            ans.add(new ArrayList<>(p));
-            return;
-        }
-        for (int i = 0; i < list.size(); i++) {
-            int num = list.get(i);
-            p.add(num);
-            list.remove(i);
-            fun(list, k, ans, p);
-            p.remove(p.size() - 1);
-            list.add(i, num);
-        }
+      List<Integer> list=new ArrayList<>();
+      int fact=1;
+      for(int i=1;i<=n;i++){
+            fact*=i;
+            list.add(i);
+      }  
+      fact/=n;
+      k-=1;
+      StringBuilder sb=new StringBuilder();
+      while(true){
+        sb.append(list.get(k / fact));
+        list.remove(k / fact);
+        if(list.size() == 0)break;
+        k%=fact;
+        fact/=list.size();
+      }
+      return String.valueOf(sb);
     }
 }
