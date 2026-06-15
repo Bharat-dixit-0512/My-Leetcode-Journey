4df316ea6c8e571848278fc135eca5d8f7bd7eab
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-06-15 23:23:45 +0530

    Time: 3 ms (98.42%) | Memory: 47.3 MB (33.11%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -3,18 +3,16 @@ class Solution {
         Integer dp[][]=new Integer[word1.length()][word2.length()];
         return fun(word1,word2,0,0,dp);
     }
-    int fun(String s1,String s2, int i, int j,Integer[][]dp){
-        if (i == s1.length()) return s2.length() - j;
-        if (j == s2.length()) return s1.length() - i;
+    int fun(String s1,String s2,int i,int j,Integer dp[][]){
+        if(i==s1.length())return s2.length()-j;
+        if(j==s2.length())return s1.length()-i;
         if(dp[i][j]!=null)return dp[i][j];
         if(s1.charAt(i)==s2.charAt(j)){
-            dp[i][j]= fun(s1,s2,i+1,j+1,dp);
-        }
+        dp[i][j]=fun(s1,s2,i+1,j+1,dp);
+        } 
         else{
-            dp[i][j]= 1+Math.min(fun(s1,s2,i,j+1,dp),Math.min(
-            fun(s1,s2,i+1,j,dp),
-            fun(s1,s2,i+1,j+1,dp)));
-        }
+            dp[i][j]=1+ Math.min(fun(s1,s2,i,j+1,dp),Math.min(fun(s1,s2,i+1,j,dp),fun(s1,s2,i+1,j+1,dp)));
+        } 
         return dp[i][j];
     }
 }
