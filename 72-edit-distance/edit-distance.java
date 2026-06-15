c6afa43da92ab73e281ee4e15377fcf8447d20c5
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-06-15 23:25:06 +0530

    Time: 5 ms (69.96%) | Memory: 46.9 MB (87.76%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,18 +1,27 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        Integer dp[][]=new Integer[word1.length()][word2.length()];
-        return fun(word1,word2,0,0,dp);
-    }
-    int fun(String s1,String s2,int i,int j,Integer dp[][]){
-        if(i==s1.length())return s2.length()-j;
-        if(j==s2.length())return s1.length()-i;
-        if(dp[i][j]!=null)return dp[i][j];
-        if(s1.charAt(i)==s2.charAt(j)){
-        dp[i][j]=fun(s1,s2,i+1,j+1,dp);
-        } 
-        else{
-            dp[i][j]=1+ Math.min(fun(s1,s2,i,j+1,dp),Math.min(fun(s1,s2,i+1,j,dp),fun(s1,s2,i+1,j+1,dp)));
-        } 
-        return dp[i][j];
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Base cases: transforming empty string to other
+        for (int i = 0; i <= m; i++) dp[i][0] = i;
+        for (int j = 0; j <= n; j++) dp[0][j] = j;
+
+        // Fill DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1]; // no operation needed
+                } else {
+                    dp[i][j] = 1 + Math.min(
+                        dp[i - 1][j],              // delete
+                        Math.min(dp[i][j - 1],     // insert
+                                 dp[i - 1][j - 1]) // replace
+                    );
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
