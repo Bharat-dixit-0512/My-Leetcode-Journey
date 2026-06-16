6a93e912693388656ac9e621fba80359eb8e4018
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-06-16 13:26:51 +0530

    Time: 10 ms (52.36%) | Memory: 47.1 MB (35.25%) - LeetSync

--- 838-design-linked-list/design-linked-list.java
+++ 838-design-linked-list/design-linked-list.java
@@ -1,28 +1,68 @@
 class MyLinkedList {
-    List<Integer> list;
+    Node head;
+    int size;
+    class Node {
+        int val;
+        Node next;
+        Node(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
     public MyLinkedList() {
-        list = new ArrayList<>();
+        head = null;
+        size = 0;
     }
-    
     public int get(int index) {
-        if(list.size() > index) return list.get(index);
-        else return -1;
+        if (index < 0 || index >= size) return -1;
+        Node curr = head;
+        for (int i = 0; i < index; i++) curr = curr.next;
+        return curr.val;
     }
-    
     public void addAtHead(int val) {
-        list.add(0, val);
+        Node nNode = new Node(val);
+        nNode.next = head;
+        head = nNode;
+        size++;
     }
-    
     public void addAtTail(int val) {
-        list.add(val);
+        Node nNode = new Node(val);
+        if (head == null) {
+            head = nNode;
+        } else {
+            Node curr = head;
+            while (curr.next != null) curr = curr.next;
+            curr.next = nNode;
+        }
+        size++;
     }
-    
     public void addAtIndex(int index, int val) {
-       if(index < list.size()) list.add(index, val);
-       else if(index == list.size()) list.add(val);
+        if (index < 0 || index > size) return;
+
+        if (index == 0) {
+            addAtHead(val);
+            return;
+        }
+        if (index == size) {
+            addAtTail(val);
+            return;
+        }
+        Node curr = head;
+        for (int i = 0; i < index - 1; i++) curr = curr.next;
+        Node nNode = new Node(val);
+        nNode.next = curr.next;
+        curr.next = nNode;
+        size++;
     }
-    
     public void deleteAtIndex(int index) {
-        if(list.size() > index) list.remove(index);
+        if (index < 0 || index >= size) return;
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node curr = head;
+            for (int i = 0; i < index - 1; i++) curr = curr.next;
+            curr.next = curr.next.next;
+        }
+        size--;
     }
 }
