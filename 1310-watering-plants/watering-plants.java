367e128caf76b533c25d832414b5cae19d127ba2
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-06-19 17:12:16 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (76.70%) - LeetSync

--- 1310-watering-plants/watering-plants.java
+++ 1310-watering-plants/watering-plants.java
@@ -1,13 +1,17 @@
 class Solution {
-    public int wateringPlants(int[] p, int capacity) {
-        int res=0,can=capacity;
-        for(int i=0;i<p.length;i++){
-            if(can<p[i]){
-                res+=i*2;
-                can=capacity;
+    public int wateringPlants(int[] plants, int capacity) {
+        int ans=0;
+        int curcapa=capacity;
+        for(int i=0;i<plants.length;i++){
+            if(curcapa>=plants[i]){
+                ans++;
+                curcapa-=plants[i];
             }
-            can-=p[i];
+            else {
+                ans+=2*i+1;
+                curcapa=capacity-plants[i];
+            }
         }
-        return res+p.length;
+        return ans;
     }
 }
