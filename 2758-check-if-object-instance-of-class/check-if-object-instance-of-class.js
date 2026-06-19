9d12245d336990db287ccf387f4633b01a39bd88
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-06-19 19:42:07 +0530

    Time: 79 ms (29.98%) | Memory: 63.5 MB (43.03%) - LeetSync

---
+++ 2758-check-if-object-instance-of-class/check-if-object-instance-of-class.js
@@ -0,0 +1,9 @@
+var checkIfInstanceOf = function(obj, classFunction) {
+    while(obj!=null){
+        if(obj.constructor===classFunction){
+            return true;
+        }
+        obj=Object.getPrototypeOf(obj);
+    }
+    return false;
+};
