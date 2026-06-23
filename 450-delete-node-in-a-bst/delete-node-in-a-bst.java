24acef6a7432301187bd257784670763dae395c2
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-06-23 13:22:59 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (5.31%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -0,0 +1,22 @@
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null)return null;
+        if(root.val>key)root.left=deleteNode(root.left,key);
+        else if(root.val<key)root.right=deleteNode(root.right,key);
+        else {
+            if(root.left==null && root.right==null)return null;
+            else if(root.left==null)return root.right;
+            else if(root.right==null)return root.left;
+            else{
+                int max=getMax(root.left);
+                root.val=max;
+                root.left=deleteNode(root.left,max);
+            }
+        }
+        return root;
+    }
+    int getMax(TreeNode root){
+        if(root.right==null)return root.val;
+        return getMax(root.right);
+    }
+}
