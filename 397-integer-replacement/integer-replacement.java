447c1e2245eb2f032765f605492f4eeb787ea927
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-06-30 20:18:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (74.95%) - LeetSync

--- 397-integer-replacement/integer-replacement.java
+++ 397-integer-replacement/integer-replacement.java
@@ -1,18 +1,15 @@
 class Solution {
-    private Map<Long, Integer> memo = new HashMap<>();
     public int integerReplacement(int n) {
-        return helper((long) n); // cast to long to avoid overflow at n+1
-    }
-    private int helper(long n) {
-        if (n == 1) return 0;
-        if (memo.containsKey(n)) return memo.get(n);
-        int steps;
-        if ((n & 1) == 0) {
-            steps = 1 + helper(n >> 1);
-        } else {
-            steps = 1 + Math.min(helper(n - 1), helper(n + 1));
+        long temp=n;
+        int c=0;
+        while(temp!=1){
+            if(temp%2==0)temp/=2;
+            else{
+                if(temp==3 || temp%4==1)temp--;
+                else temp++;
+            }
+            c++;
         }
-        memo.put(n, steps);
-        return steps;
+        return c;
     }
 }
