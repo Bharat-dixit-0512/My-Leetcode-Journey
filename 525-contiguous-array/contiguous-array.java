9c4a98b6b1f95d1797eaa80bf478fac3795ebcd3
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-07-11 11:06:34 +0530

    Time: 24 ms (56.94%) | Memory: 65.2 MB (87.71%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        Map<Integer,Integer> map=new HashMap<>();
+        int subArrayLength=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i]==0?-1:1;
+            if(sum==0){
+                subArrayLength=i+1;
+
+            }else if(map.containsKey(sum)){
+                subArrayLength=Math.max(subArrayLength,i-map.get(sum));
+            }else{
+                map.put(sum,i);
+            }
+        }
+        return subArrayLength;
+    }
+}
