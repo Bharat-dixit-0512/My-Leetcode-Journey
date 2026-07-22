6c3cdda2004f6e8bf17403bfb609d9adae8b3c70
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-07-22 23:48:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (45.25%) - LeetSync

--- 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.java
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.java
@@ -1,5 +1,14 @@
 class Solution {
     public String toHex(int num) {
-        return Integer.toHexString(num);
+        if (num == 0) return "0";
+        char[] s = "0123456789abcdef".toCharArray();
+        StringBuilder sb = new StringBuilder();
+        
+        while (num != 0) {
+            int rem = num & 15;//behaves num%16 but faster and also work for negative values
+            sb.append(s[rem]);
+            num >>>= 4; //it behaves like num/=4
+        }
+        return sb.reverse().toString();
     }
 }
