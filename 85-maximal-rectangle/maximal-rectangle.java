fa62ccf879f02ac3844aa2891fbcd9d2b5f7ebe8
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-07-24 15:13:48 +0530

    Time: 32 ms (16.04%) | Memory: 51.7 MB (14.41%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return 0;
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[][] mat = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                mat[i][j] = matrix[i][j] - '0';
+            }
+        }
+
+        for(int i=0;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(mat[i][j]==1){
+                    mat[i][j]+=mat[i][j-1];
+                }
+            }
+        }
+        int ans=0;
+
+        for(int j=0;j<n;j++){
+            for(int i=0;i<m;i++){
+                int width=mat[i][j];
+                if(width==0)continue;
+
+                int currW=width;
+                for(int k=i;k<m && mat[k][j]>0;k++){
+                    currW=Math.min(currW,mat[k][j]);
+                    int height=k-i+1;
+                    ans=Math.max(ans,currW*height);
+                }
+
+                currW=width;
+                for(int k=i;k>=0 && mat[k][j]>0;k--){
+                    currW=Math.min(currW,mat[k][j]);
+                    int height=i-k+1;
+                    ans=Math.max(ans,currW*height);
+                }
+            }
+        }
+        return ans;
+    }
+}
