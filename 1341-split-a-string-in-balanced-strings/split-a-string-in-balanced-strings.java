67a56a718657bb7de5c926c5c3cf9b9784e3cf31
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-07-25 11:27:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (91.60%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int count=0,balance=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='L'){
+                balance++;
+            }
+            else{
+                balance--;
+            }
+            if(balance==0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
