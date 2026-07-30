76af2bc29f38c716be7d07b3e2ffab7d73541090
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-07-30 11:50:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (66.51%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -1,7 +1,10 @@
 class Solution {
     public int minimumPushes(String word) {
-        int n=word.length();
-        int blocks=n/8;
-        return (blocks *(blocks+1)*4)+(n%8)*(blocks+1);
+        int n = word.length();
+        int pushCnt = 0;
+        for(int i = 0; i < n; i++){
+            pushCnt += i / 8 + 1;
+        }
+        return pushCnt;
     }
 }
