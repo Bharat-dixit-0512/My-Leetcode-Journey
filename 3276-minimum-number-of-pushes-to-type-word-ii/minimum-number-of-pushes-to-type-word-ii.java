6814c6d97d514df2d541a866f96078f7a22f33ec
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-07-31 22:26:28 +0530

    Time: 9 ms (94.22%) | Memory: 48 MB (73.70%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -6,12 +6,18 @@ class Solution {
         }
 
         Arrays.sort(freq);
-        int res = 0, idx = 0;
+        int res = 0;
         for (int i = 25; i >= 0; i--) {
             if (freq[i] == 0) break;
-            res += freq[i] * (idx / 8 + 1);
-            idx++;
+            res += freq[i] * (((25-i) / 8) + 1);
         }
         return res;
     }
 }
+
+// abcde
+// a-1 b-1 c-1 d-1 e-1
+
+// 1*(0+1)=1 res=1 idx=1;
+// 1*(0+1)=1 res=2 idx=2;
+// 1*(0+1)=1 res=3 idx=3;
