0cf2fc8b17cbd611d6699bbe2746e40eb7709e11
Bharat Dixit <167958266+Bharat-dixit-0512@users.noreply.github.com>
2026-08-16 20:49:09 +0530

    Time: 50 ms (46.80%) | Memory: 52.3 MB (72.50%) - LeetSync

--- 1582-design-browser-history/design-browser-history.java
+++ 1582-design-browser-history/design-browser-history.java
@@ -8,20 +8,16 @@ class BrowserHistory {
             prev=null;
         }
     }
-
     Node curr;
     public BrowserHistory(String homepage) {
         curr=new Node(homepage);
     }
-    
     public void visit(String url) {
         Node node=new Node(url);
         curr.next=node;
         node.prev=curr;
         curr=node;
-    }
-
-    
+    } 
     public String back(int steps) {
         while(curr.prev!=null && steps>0){
             curr=curr.prev;
@@ -29,7 +25,6 @@ class BrowserHistory {
         }
         return curr.url;
     }
-    
     public String forward(int steps) {
         while(curr.next!=null && steps>0){
             curr=curr.next;
